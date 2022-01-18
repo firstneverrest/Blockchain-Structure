4713b9fcc2c9e0363b27082100d6896766523af8
Chitsanupong <first30952@gmail.com>
2022-01-18 13:37:04 +0700

    feat: add hash function

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,12 @@
 import datetime
+import hashlib
+import json
 
 class blockchain:
     def __init__(self):
         self.chain = [] # list of blocks
         self.add_block(nonce=1, previous_hash='0')
+        self.add_block(nonce=3, previous_hash='12')
 
     def add_block(self, nonce, previous_hash):
         block = {
@@ -15,5 +18,14 @@ class blockchain:
         self.chain.append(block)
         return block
 
+    # return the last block in the chain
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    # hash the block (sha256) 64 characters
+    def hash(self, block):
+        encode_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encode_block).hexdigest()
+
 blockchain = blockchain()
-print(blockchain.chain)
+print(blockchain.hash(blockchain.chain[0]))
